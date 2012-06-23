b70660972aaa6c7090e8fe794a0b0a333d353fb6
screencast <screencast@imotep.monheim.office.innoq.com>
2012-06-23 17:12:09 +0200

    updating table styling

--- app/models/Team.java
+++ app/models/Team.java
@@ -14,12 +14,10 @@ import play.db.jpa.Model;
 @Entity
 public class Team extends Model {
 
-    @ManyToMany(cascade=CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     public Set<Player> players;
-
     @OneToMany(mappedBy = "team1")
     public Set<Match> matches1;
-
     @OneToMany(mappedBy = "team2")
     public Set<Match> matches2;
 
@@ -32,9 +30,13 @@ public class Team extends Model {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        int i = 0;
         for (Player p : players) {
             sb.append(p.name);
-            sb.append(" ");
+            i++;
+            if (i < players.size()) {
+                sb.append(" & ");
+            }
         }
         return sb.toString();
     }
