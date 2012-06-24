e309da278f52533bbd2a3186490008ed805de697
Philipp Haußleiter <philipp@haussleiter.de>
2012-06-24 13:19:09 +0200

    updating Application index file, fixing date format in match creation.

--- app/models/Match.java
+++ app/models/Match.java
@@ -1,10 +1,13 @@
 package models;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 /**
@@ -13,17 +16,43 @@ import play.db.jpa.Model;
  */
 @Entity
 public class Match extends Model {
+
     @ManyToOne
+    @Required
     public Team team1;
     @ManyToOne
+    @Required
     public Team team2;
     public int scoreTeam1;
     public int scoreTeam2;
-
     @Temporal(TemporalType.DATE)
     public Date date;
-    
-    public Match(){
+
+    public Match() {
         date = new Date();
+    }
+
+    public Set<Player> getPlayers() {
+        Set<Player> players = new HashSet<Player>();
+        for (Player p : team1.players) {
+            players.add(p);
+        }
+        for (Player p : team2.players) {
+            players.add(p);
+        }
+        return players;
+    }
+
+    public boolean isOpen(){
+        return getPlayers().size() < 4;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Player p : getPlayers()){
+            sb.append(p).append(" ");
+        }
+        return sb.toString();
     }
 }
